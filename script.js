ca82d319bc4a969cd749b493872e43fb36334f5a
rmango <morgan-graham@live.com>
2018-11-20 15:58:25 -0800

    adding sticky header

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Im running");
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+window.onscroll = function() {myFunction()};
+
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
